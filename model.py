187963b156f772e09a129c59594b34cb0d00e6a6
Mohamed El Sobky <mohamed.elsobky@valeo.com>
2017-11-02 03:35:48 +0200

    added a dropout layer and applied some data augmentation

--- model.py
+++ model.py
@@ -9,60 +9,6 @@ from keras.models import Sequential
 from keras.layers import Flatten,Dense,Conv2D,Activation,MaxPooling2D,Dropout,Lambda,Cropping2D
 
 
-def load_Training_data(data_dir_path):
-    lines = []
-    correction_factor = 0.20
-    with open(data_dir_path + 'driving_log.csv') as csvfile:
-        reader = csv.reader(csvfile)
-        for line in reader:
-            lines.append(line)
-    images = []
-    measurements = []
-    for index,line in enumerate(lines):
-        print("Loading image set {} / {}".format(index,len(lines)-1))
-        if index == 0: continue
-        path = data_dir_path + 'IMG/'
-        center_filename = line[0].split('\\')[-1]
-        left_filename = line[1].split('\\')[-1]
-        right_filename = line[2].split('\\')[-1]
-
-        c_image = cv2.imread(path + center_filename)
-        l_image = cv2.imread(path + left_filename)
-        r_image = cv2.imread(path + right_filename)
-
-        c_measurement = float(line[3])
-        l_measurement = c_measurement + correction_factor
-        r_measurement = c_measurement - correction_factor
-
-        c_image_flipped = np.fliplr(c_image)
-        l_image_flipped = np.fliplr(l_image)
-        r_image_flipped = np.fliplr(r_image)
-        c_measurement_flipped = - c_measurement
-        l_measurement_flipped = - l_measurement
-        r_measurement_flipped = - r_measurement
-
-        images.append(c_image)
-        images.append(l_image)
-        images.append(r_image)
-        images.append(c_image_flipped)
-        images.append(l_image_flipped)
-        images.append(r_image_flipped)
-
-
-        measurements.append(c_measurement)
-        measurements.append(l_measurement)
-        measurements.append(r_measurement)
-        measurements.append(c_measurement_flipped)
-        measurements.append(l_measurement_flipped)
-        measurements.append(r_measurement_flipped)
-
-    X_train = np.array(images)
-    y_train = np.array(measurements)
-    return X_train,y_train
-
-
-
-
 def load_data_samples(datapath):
     samples = []
     with open(datapath) as csvfile:
@@ -91,26 +37,38 @@ def generator(samples, batch_size=32):
                 c_image = cv2.imread(center_filename)
                 l_image = cv2.imread(left_filename)
                 r_image = cv2.imread(right_filename)
-
+                c_image_flipped = np.fliplr(c_image)
+                l_image_flipped = np.fliplr(l_image)
+                r_image_flipped = np.fliplr(r_image)
 
                 c_measurement = float(batch_sample[3])
                 l_measurement = c_measurement + correction_factor
                 r_measurement = c_measurement - correction_factor
 
+                c_measurement_flipped = - c_measurement
+                l_measurement_flipped = - l_measurement
+                r_measurement_flipped = - r_measurement
+
                 images.append(c_image)
                 images.append(l_image)
                 images.append(r_image)
+                images.append(c_image_flipped)
+                images.append(l_image_flipped)
+                images.append(r_image_flipped)
 
                 angles.append(c_measurement)
                 angles.append(l_measurement)
                 angles.append(r_measurement)
+                angles.append(c_measurement_flipped)
+                angles.append(l_measurement_flipped)
+                angles.append(r_measurement_flipped)
 
             # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-train_samples, validation_samples = load_data_samples('../data/Long1/driving_log.csv')
+train_samples, validation_samples = load_data_samples('../data/Long2/driving_log.csv')
 
 # compile and train the model using the generator function
 train_generator = generator(train_samples, batch_size=32)
@@ -129,12 +87,13 @@ model.add(Conv2D(64,3,3,activation="relu"))
 model.add(Flatten())
 
 model.add(Dense(100))
+model.add(Dropout(0.5))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse',optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples)*6, validation_data=validation_generator, nb_val_samples=len(validation_samples)*6, nb_epoch=2)
 
 model.save('model.h5')
 
